0aac5556d86b0c26af9b9016ec7d968399413add
Vladimír Forman <forman.vl@gmail.com>
2024-08-18 09:55:49 +0200

    Přidaní kalibračních dat do EEPROM

--- ESP32-Mqtt.ino
+++ ESP32-Mqtt.ino
@@ -17,7 +17,7 @@
 //                  Rozdělení světel a relé
 //
 // ESP32 desky - https://dl.espressif.com/dl/package_esp32_index.json
-// 
+//
 //
 // ESP 32 Wroom mini S2 - Deneyap Mini
 // Mikrofon (KY-038) VCC → 3.3V , GNG → GND , A0 → GPIO34
@@ -91,65 +91,65 @@
 //
 
 
-#include <PubSubClient.h>         // https://github.com/knolleary/pubsubclient
-#include <WiFi.h>                 // https://github.com/espressif/arduino-esp32/tree/master/libraries/WiFi
-#include <WiFiManager.h>          // https://github.com/tzapu/WiFiManager
-#include <ArduinoJson.h>          // https://github.com/bblanchon/ArduinoJson
-#include <DHT.h>                  // https://github.com/adafruit/DHT-sensor-library
-#include <Preferences.h>          // https://github.com/espressif/arduino-esp32/tree/master/libraries/Preferences
+#include <PubSubClient.h>  // https://github.com/knolleary/pubsubclient
+#include <WiFi.h>          // https://github.com/espressif/arduino-esp32/tree/master/libraries/WiFi
+#include <WiFiManager.h>   // https://github.com/tzapu/WiFiManager
+#include <ArduinoJson.h>   // https://github.com/bblanchon/ArduinoJson
+#include <DHT.h>           // https://github.com/adafruit/DHT-sensor-library
+#include <Preferences.h>   // https://github.com/espressif/arduino-esp32/tree/master/libraries/Preferences
 
-#define DHTTYPE DHT11             // Typ DHT sezoru teploty a vlhkosti
-#define PREF_NAMESPACE "mqtt-app" // Jmenný prostor EEPROM
-uint8_t DHTPin = 3;               // Nastavení datového pinu DHT
-DHT dht(DHTPin, DHTTYPE);         // Inicializace DHT senzoru
+#define DHTTYPE DHT11              // Typ DHT sezoru teploty a vlhkosti
+#define PREF_NAMESPACE "mqtt-app"  // Jmenný prostor EEPROM
+uint8_t DHTPin = 3;                // Nastavení datového pinu DHT
+DHT dht(DHTPin, DHTTYPE);          // Inicializace DHT senzoru
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 Preferences preferences;
 
 // Rozdělit světla a relé
-const int LedType = 3;            // !! CHANGE !!  Typ led světel ( White(1) - 1 / Wihe(2) - 2 / White (3) - 4 / RGB - 8)
-#define WIFI_HOSTNAME "ESP32_12"  // !! CHANGE !! Název zařízení v síti
-const String Svetlo = "Svetlo_05";// !! CHANGE !!  Topic název zařízení
-const bool Relay = false;         // !! CHANGE !!  Relé (Zásuvka)
-const bool Clap = false;          // !! CHANGE !!  Použití mikrofonu
-const bool Temp = true;           // !! CHANGE !!  Použití DHT sezoru měření teploty
-const int Stisk = 7;              // !! CHANGE !!  Použití tlačítka                   // Led světlo 1 - 1 , Led svěetlo 2 - 2 , Led světlo 3 / RGB - 4 , Relé - 16
-const bool AmpMeter = false;      // !! CHANGE !!  Zapnutí měření odběru
+const int LedType = 3;              // !! CHANGE !!  Typ led světel   ( White(1) - 1 / Wihe(2) - 2 / White (3) - 4 / RGB - 8 )
+#define WIFI_HOSTNAME "ESP32_12"    // !! CHANGE !!  Název zařízení v síti
+const String Svetlo = "Svetlo_05";  // !! CHANGE !!  Topic název zařízení
+const bool Relay = false;           // !! CHANGE !!  Relé (Zásuvka)
+const bool Clap = false;            // !! CHANGE !!  Použití mikrofonu
+const bool Temp = true;             // !! CHANGE !!  Použití DHT sezoru měření teploty
+const int Stisk = 7;                // !! CHANGE !!  Použití tlačítka ( Led světlo 1 - 1 , Led světlo 2 - 2 , Led světlo 3 / RGB - 4 , Relé - 16 )
+const bool AmpMeter = false;        // !! CHANGE !!  Zapnutí měření odběru
 
-char ssid[32];                    // Proměnná pro SSID
-char password[32];                // Proměnná pro heslo
+char ssid[32];      // Proměnná pro SSID
+char password[32];  // Proměnná pro heslo
 // Mqtt proměnné nastavení
-char mqtt_server[40];             // MQTT IP adress
-char mqtt_port[6] = "1883";       // MQTT port
-char mqtt_username[32];           // MQTT User name
-char mqtt_password[32];           // MQTT Password
+char mqtt_server[40];        // MQTT IP adress
+char mqtt_port[6] = "1883";  // MQTT port
+char mqtt_username[32];      // MQTT User name
+char mqtt_password[32];      // MQTT Password
 // Nastavení vstupů ESP
-const int Sw = 2;                 // Tlačítko
-const int ClapSensor = 12;        // Zvukový senzor připojený na analogový vstup GPIO34
-const int AmpPin = 40;            // Vstup ampérmetru
+const int Sw = 2;           // Tlačítko
+const int ClapSensor = 12;  // Zvukový senzor připojený na analogový vstup GPIO34
+const int AmpPin = 40;      // Vstup ampérmetru
 // Výstupy kontrolky led
-const int LedPWR = 11;            // Ledka power
-const int LedWi = 7;              // Ledka připojení
-const int PwrSw = 9;              // Ledka / transistor zapnuti
-// Výstupy ovládání 
-const int Re = 39;                // Relé
-const int PwrRed = 4;             // Ledka power (red)   / Světlo 1
-const int PwrGreen = 6;           // Ledka power (green) / Světlo 2
-const int PwrBlue = 8;            // Ledka power (blue)  / Světlo 3
+const int LedPWR = 11;  // Ledka power
+const int LedWi = 7;    // Ledka připojení
+const int PwrSw = 9;    // Ledka / transistor zapnuti
+// Výstupy ovládání
+const int Re = 39;       // Relé
+const int PwrRed = 4;    // Ledka power (red)   / Světlo 1
+const int PwrGreen = 6;  // Ledka power (green) / Světlo 2
+const int PwrBlue = 8;   // Ledka power (blue)  / Světlo 3
 
-int ClapThreshold = 900;          // Nastavitelná hladina detekce tlesknutí
-int CekejOdeslat = 16000;         // Prodleva mezi odesláním naměřených hodnot
-int CekejMereniDHT = 6400;        // Prodleva mezi měřením DHT
-int CekejDetectClap = 50;         // Prodleva mezi detekcí tlesknutí
+int ClapThreshold = 900;    // Nastavitelná hladina detekce tlesknutí
+int CekejOdeslat = 16000;   // Prodleva mezi odesláním naměřených hodnot
+int CekejMereniDHT = 6400;  // Prodleva mezi měřením DHT
+int CekejDetectClap = 50;   // Prodleva mezi detekcí tlesknutí
 long LastMsg = 0;
 int Value = 0;
 char SvetloChr[50];
 bool Tlac;
 bool Rep;
 String Zap_str;
-int OZap;                         // Led světlo 1 - 1 , Led svěetlo 2 - 2 , Led světlo 3 - 4 , RGB - 8 , Relé - 16
-int Zap;                          // Led světlo 1 - 1 , Led svěetlo 2 - 2 , Led světlo 3 - 4 , RGB - 8 , Relé - 16
+int OZap;  // Led světlo 1 - 1 , Led svěetlo 2 - 2 , Led světlo 3 - 4 , RGB - 8 , Relé - 16
+int Zap;   // Led světlo 1 - 1 , Led svěetlo 2 - 2 , Led světlo 3 - 4 , RGB - 8 , Relé - 16
 float Teplota;
 float Vlhkost;
 char Pwr[50];
@@ -168,32 +168,64 @@ const unsigned long doubleClapWindow = 500;
 int TimerOdeslat, TimerMereni;
 
 void setup() {
-  pinMode(Sw, INPUT_PULLUP);      // PullUp výstup nastavení pinu pro Tlačítko
-  pinMode(LedPWR, OUTPUT);        // Výstup nastavení pinu pro kontrolku led (červená) power
-  pinMode(LedWi, OUTPUT);         // Výstup nastavení pinu pro kontrolku led (modrá)   připojení k WiFi a MQTT
-  pinMode(PwrSw, OUTPUT);         // Výstup nastavení pinu pro kontrolku led (zelená)  zapnutí/vypnutí led/relé
-  pinMode(Re, OUTPUT);            // Výstup nastavení pinu pro relé
-  pinMode(PwrRed, OUTPUT);        // Výstup pro nastavení pinu světla led (red   / white)
-  pinMode(PwrGreen, OUTPUT);      // Výstup pro nastavení pinu světla led (green / white)
-  pinMode(PwrBlue, OUTPUT);       // Výstup pro nastavení pinu světla led (blue  / white)
-  pinMode(ClapSensor, INPUT);     // Vstup pro nastavení pinu Mikrofon
-  pinMode(AmpPin,INPUT);          // Vstup pro nastavení pinu Ampermetr
+  pinMode(Sw, INPUT_PULLUP);   // PullUp výstup nastavení pinu pro Tlačítko
+  pinMode(LedPWR, OUTPUT);     // Výstup nastavení pinu pro kontrolku led (červená) power
+  pinMode(LedWi, OUTPUT);      // Výstup nastavení pinu pro kontrolku led (modrá)   připojení k WiFi a MQTT
+  pinMode(PwrSw, OUTPUT);      // Výstup nastavení pinu pro kontrolku led (zelená)  zapnutí/vypnutí led/relé
+  pinMode(Re, OUTPUT);         // Výstup nastavení pinu pro relé
+  pinMode(PwrRed, OUTPUT);     // Výstup pro nastavení pinu světla led (red   / white)
+  pinMode(PwrGreen, OUTPUT);   // Výstup pro nastavení pinu světla led (green / white)
+  pinMode(PwrBlue, OUTPUT);    // Výstup pro nastavení pinu světla led (blue  / white)
+  pinMode(ClapSensor, INPUT);  // Vstup pro nastavení pinu Mikrofon
+  pinMode(AmpPin, INPUT);      // Vstup pro nastavení pinu Ampermetr
 
-  analogWrite(LedPWR, LedL);      // Zapnutí kontrolky (červená) připojení ke zdroji
+  analogWrite(LedPWR, LedL);  // Zapnutí kontrolky (červená) připojení ke zdroji
   Serial.begin(115200);
   delay(1500);
   if (Temp) {
     dht.begin();
   }
   preferences.begin(PREF_NAMESPACE, false);
-  
+
+  // Kontrola nastavených kalibračních hodnot v EEPROM
+  if (!preferences.isKey("KalibrT")) {
+    preferences.putDouble("KalibrT", 1.33);
+  }
+  KalibrT = preferences.getDouble("KalibrT", 1.33);
+
+  if (!preferences.isKey("KalibrV")) {
+    preferences.putDouble("KalibrV", 0.70);
+  }
+  KalibrV = preferences.getDouble("KalibrV", 0.70);
+
+  if (!preferences.isKey("ClapThreshold")) {
+    preferences.putInt("ClapThreshold", 900);
+  }
+  ClapThreshold = preferences.getInt("ClapThreshold", 900);
+
+  if (!preferences.isKey("CekejOdeslat")) {
+    preferences.putInt("CekejOdeslat", 16000);
+  }
+  CekejOdeslat = preferences.getInt("CekejOdeslat", 16000);
+
+  if (!preferences.isKey("CekejMereniDHT")) {
+    preferences.putInt("CekejMereniDHT", 6400);
+  }
+  CekejMereniDHT = preferences.getInt("CekejMereniDHT", 6400);
+
+  if (!preferences.isKey("CekejDetectClap")) {
+    preferences.putInt("CekejDetectClap", 50);
+  }
+  CekejDetectClap = preferences.getInt("CekejDetectClap", 50);
+
+
   // Načtení uložených hodnot
   String savedServer = preferences.getString("mqtt_server", "");
   String savedPort = preferences.getString("mqtt_port", "");
-  
+
   savedServer.toCharArray(mqtt_server, sizeof(mqtt_server));
   savedPort.toCharArray(mqtt_port, sizeof(mqtt_port));
-  
+
   WiFiManager wifiManager;
 
   // Pokud chcete vymazat všechny uložené informace wifiManageru, odkomentujte následující řádku a spusťte jej jednou
@@ -247,7 +279,7 @@ void setup() {
 }
 
 void detectClap() {
- int clapValue = analogRead(ClapSensor);
+  int clapValue = analogRead(ClapSensor);
   unsigned long currentTime = millis();
 
   if (clapValue > ClapThreshold) {
@@ -260,13 +292,13 @@ void detectClap() {
       if (currentTime - lastClapTime <= doubleClapWindow) {
         // Dvojtlesk detekován
         Zap = !Zap;
-        firstClapDetected = false; // Reset detekce
+        firstClapDetected = false;  // Reset detekce
       } else {
         // Příliš dlouhý interval, považujeme za nový první tlesk
         lastClapTime = currentTime;
       }
     }
-    delay(CekejDetectClap); // Zpoždění pro zabránění falešných detekcí
+    delay(CekejDetectClap);  // Zpoždění pro zabránění falešných detekcí
   } else if (firstClapDetected && (currentTime - lastClapTime > doubleClapWindow)) {
     // Vypršelo časové okno pro druhé tlesknutí
     firstClapDetected = false;
@@ -412,22 +444,49 @@ void callbackSettings(JsonDocument& doc) {
     }
   } else if (strcmp(settingAction, "get") == 0) {
     StaticJsonDocument<256> responseDoc;
-    responseDoc["ClapThreshold"] = ClapThreshold;
-    responseDoc["CekejOdeslat"] = CekejOdeslat;
-    responseDoc["CekejMereniDHT"] = CekejMereniDHT;
-    responseDoc["CekejDetectClap"] = CekejDetectClap;
-    responseDoc["KalibrT"] = KalibrT;
-    responseDoc["KalibrV"] = KalibrV;
+    if (strcmp(settingAction, "set") == 0) {
+      if (doc["ClapThreshold"] != nullptr) {
+        ClapThreshold = doc["ClapThreshold"].as<int>();
+        preferences.putInt("ClapThreshold", ClapThreshold);
+      }
+      if (doc["CekejOdeslat"] != nullptr) {
+        CekejOdeslat = doc["CekejOdeslat"].as<int>();
+        preferences.putInt("CekejOdeslat", CekejOdeslat);
+      }
+      if (doc["CekejMereniDHT"] != nullptr) {
+        CekejMereniDHT = doc["CekejMereniDHT"].as<int>();
+        preferences.putInt("CekejMereniDHT", CekejMereniDHT);
+      }
+      if (doc["CekejDetectClap"] != nullptr) {
+        CekejDetectClap = doc["CekejDetectClap"].as<int>();
+        preferences.putInt("CekejDetectClap", CekejDetectClap);
+      }
+      if (doc["KalibrT"] != nullptr) {
+        KalibrT = doc["KalibrT"].as<double>();
+        preferences.putDouble("KalibrT", KalibrT);
+      }
+      if (doc["KalibrV"] != nullptr) {
+        KalibrV = doc["KalibrV"].as<double>();
+        preferences.putDouble("KalibrV", KalibrV);
+      }
+    } else if (strcmp(settingAction, "get") == 0) {
+      StaticJsonDocument<256> responseDoc;
+      responseDoc["ClapThreshold"] = ClapThreshold;
+      responseDoc["CekejOdeslat"] = CekejOdeslat;
+      responseDoc["CekejMereniDHT"] = CekejMereniDHT;
+      responseDoc["CekejDetectClap"] = CekejDetectClap;
+      responseDoc["KalibrT"] = KalibrT;
+      responseDoc["KalibrV"] = KalibrV;
 
-    char responseOut[256];
-    serializeJson(responseDoc, responseOut);
-    client.publish(SvetloChr, responseOut);  // Předpokládáme, že existuje MQTT klient
-    return;
+      char responseOut[256];
+      serializeJson(responseDoc, responseOut);
+      client.publish(SvetloChr, responseOut);
+      return;
+    }
   }
 }
-
-void callbackDevice(JsonDocument& doc){
-  if (doc["on"] != nullptr) {
+  void callbackDevice(JsonDocument & doc) {
+    if (doc["on"] != nullptr) {
       Zap = doc["on"];
     }
     if (doc["spectrumRGB"][0] != nullptr) {
@@ -450,124 +509,133 @@ void callbackDevice(JsonDocument& doc){
     if (doc["stav"] != nullptr) {
       Poslat();
     }
-}
-
-void reconnect() {
-  if (!client.connected()) {
-    IsConnected = false;
   }
-}
 
-void Poslat() {
-  DynamicJsonDocument doc(256);
-  doc["on"] = Zap;
-  doc["ip"] = WiFi.localIP().toString();
-  doc["host"] = WIFI_HOSTNAME;
-  doc["signal"] = WiFi.RSSI();
-  doc["brightArd"] = LedL;
-  if (LedType == 1) {
-    doc["brightness"] = round(Bright / 2.54);
-  } else if (LedType == 2) {
-    JsonArray data = doc.createNestedArray("spectrumRGB");
-    data.add(Red);
-    data.add(Green);
-    data.add(Blue);
-  }
-  if (Temp) {
-    senzorTemp();
-    delay(50);
-    doc["temp"] = Teplota;
-    doc["hum"] = Vlhkost;
-
-    Serial.print("Teplota je ");
-    Serial.print(Teplota);
-    Serial.print(" °C a vlhkost je ");
-    Serial.print(Vlhkost);
-    Serial.println(" %RH");
-  }
-  char out[256];
-  int jsonSize = serializeJson(doc, out);
-  // Přidejte velikost JSON do dokumentu
-  doc["JSONsize"] = jsonSize;
-  // Serialize znovu s délkou JSON
-  serializeJson(doc, out);
-  client.publish(SvetloChr, out);
-}
-
-void Push() {
-  Tlac = digitalRead(Sw);  // Tlačítko
-  if (!Tlac) {
-    if (!Rep) {
-      if (Zap == 0) {
-        Zap = Stisk;
-      } else {
-        Zap = 0;
-      }
-      Rep = true;
-    }
-  } else {
-    Rep = false;
-  }
-}
-
-void senzorTemp() {
-  Teplota = (Teplota + dht.readTemperature() / KalibrT) / 2;
-  delay(100);
-  Vlhkost = (Vlhkost + dht.readHumidity() / KalibrV) / 2;
-  delay(100);
-}
-
-void measureAmp() {  // Měření hodnoty z ampermetru
-}
-
-void connectToNetwork() {
-int n = WiFi.scanNetworks();
-  int bestNetworkIndex = -1;
-  int bestRSSI = -9999;           // Nízká výchozí hodnota pro porovnání
-
-  for (int i = 0; i < n; i++) {
-    if (WiFi.SSID(i) == ssid) {   // Hledání sítě s požadovaným SSID
-      int rssi = WiFi.RSSI(i);
-      if (rssi > bestRSSI) {      // Pokud je signál silnější, ulož index a RSSI
-        bestNetworkIndex = i;
-        bestRSSI = rssi;
-      }
+  void reconnect() {
+    if (!client.connected()) {
+      IsConnected = false;
     }
   }
 
-  if (bestNetworkIndex != -1) {   // Pokud byla nalezena vhodná síť
-    WiFi.begin(WiFi.SSID(bestNetworkIndex).c_str(), password);
+  void Poslat() {
+    DynamicJsonDocument doc(256);
+    doc["on"] = Zap;
+    doc["ip"] = WiFi.localIP().toString();
+    doc["host"] = WIFI_HOSTNAME;
+    doc["signal"] = WiFi.RSSI();
+    doc["brightArd"] = LedL;
 
-    int timeout = 10000;          // 10 sekund timeout
-    unsigned long startAttemptTime = millis();
+    // Přidání kalibračních hodnot a časovačů
+    doc["KalibrT"] = KalibrT;
+    doc["KalibrV"] = KalibrV;
+    doc["ClapThreshold"] = ClapThreshold;
+    doc["CekejOdeslat"] = CekejOdeslat;
+    doc["CekejMereniDHT"] = CekejMereniDHT;
+    doc["CekejDetectClap"] = CekejDetectClap;
 
-    while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < timeout) {
-      analogWrite(LedWi, HIGH);
+    if (LedType == 1) {
+      doc["brightness"] = round(Bright / 2.54);
+    } else if (LedType == 2) {
+      JsonArray data = doc.createNestedArray("spectrumRGB");
+      data.add(Red);
+      data.add(Green);
+      data.add(Blue);
+    }
+    if (Temp) {
+      senzorTemp();
       delay(50);
-      analogWrite(LedWi, LOW);
-      delay(50);
+      doc["temp"] = Teplota;
+      doc["hum"] = Vlhkost;
+
+      Serial.print("Teplota je ");
+      Serial.print(Teplota);
+      Serial.print(" °C a vlhkost je ");
+      Serial.print(Vlhkost);
+      Serial.println(" %RH");
     }
+    char out[256];
+    int jsonSize = serializeJson(doc, out);
+    // Přidejte velikost JSON do dokumentu
+    doc["JSONsize"] = jsonSize;
+    // Serialize znovu s délkou JSON
+    serializeJson(doc, out);
+    client.publish(SvetloChr, out);
+  }
 
-    if (WiFi.status() == WL_CONNECTED) {
-      analogWrite(LedWi, LedL);
-
-      while (!client.connected()) {
-        if (client.connect(WIFI_HOSTNAME)) {
-          analogWrite(LedWi, LedL);
-          client.subscribe(SvetloChr);
-          IsConnected = true;
-          return;                 // MQTT připojeno, ukončit funkci
+  void Push() {
+    Tlac = digitalRead(Sw);  // Tlačítko
+    if (!Tlac) {
+      if (!Rep) {
+        if (Zap == 0) {
+          Zap = Stisk;
         } else {
-          analogWrite(LedWi, LedL);
-          delay(2500);
-          analogWrite(LedWi, 0);
-          delay(2500);
+          Zap = 0;
         }
+        Rep = true;
+      }
+    } else {
+      Rep = false;
+    }
+  }
+
+  void senzorTemp() {
+    Teplota = (Teplota + dht.readTemperature() / KalibrT) / 2;
+    delay(100);
+    Vlhkost = (Vlhkost + dht.readHumidity() / KalibrV) / 2;
+    delay(100);
+  }
+
+  void measureAmp() {  // Měření hodnoty z ampermetru
+  }
+
+  void connectToNetwork() {
+    int n = WiFi.scanNetworks();
+    int bestNetworkIndex = -1;
+    int bestRSSI = -9999;  // Nízká výchozí hodnota pro porovnání
+
+    for (int i = 0; i < n; i++) {
+      if (WiFi.SSID(i) == ssid) {  // Hledání sítě s požadovaným SSID
+        int rssi = WiFi.RSSI(i);
+        if (rssi > bestRSSI) {  // Pokud je signál silnější, ulož index a RSSI
+          bestNetworkIndex = i;
+          bestRSSI = rssi;
+        }
+      }
+    }
+
+    if (bestNetworkIndex != -1) {  // Pokud byla nalezena vhodná síť
+      WiFi.begin(WiFi.SSID(bestNetworkIndex).c_str(), password);
+
+      int timeout = 10000;  // 10 sekund timeout
+      unsigned long startAttemptTime = millis();
+
+      while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < timeout) {
+        analogWrite(LedWi, HIGH);
+        delay(50);
+        analogWrite(LedWi, LOW);
+        delay(50);
+      }
+
+      if (WiFi.status() == WL_CONNECTED) {
+        analogWrite(LedWi, LedL);
+
+        while (!client.connected()) {
+          if (client.connect(WIFI_HOSTNAME)) {
+            analogWrite(LedWi, LedL);
+            client.subscribe(SvetloChr);
+            IsConnected = true;
+            return;  // MQTT připojeno, ukončit funkci
+          } else {
+            analogWrite(LedWi, LedL);
+            delay(2500);
+            analogWrite(LedWi, 0);
+            delay(2500);
+          }
+        }
+      } else {
+        IsConnected = false;
       }
     } else {
       IsConnected = false;
     }
-  } else {
-    IsConnected = false;
   }
-}
