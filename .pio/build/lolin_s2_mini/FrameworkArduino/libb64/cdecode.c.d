1653a2071c62393e7cbb9c5a06beaf0610d9d452
Vladimír Forman <forman@e-apollo.cz>
2026-02-12 15:09:49 +0100

    .pio update

---
+++ .pio/build/lolin_s2_mini/FrameworkArduino/libb64/cdecode.c.d
@@ -0,0 +1,3 @@
+.pio/build/lolin_s2_mini/FrameworkArduino/libb64/cdecode.c.o: \
+ C:/Users/forman.APOLLOCZ/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
+ C:/Users/forman.APOLLOCZ/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
